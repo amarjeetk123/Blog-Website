8949ccc2e5be0157d34bbba4e3ae8a0b6903ef6a
amarjeetk123 <ak123del@gmail.com>
2022-12-09 01:18:58 +0530

    adding one more controller for geting any post by postid

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -153,7 +153,7 @@ exports.deleteuser = async (req, res) => {
     if (req.body.userId === req.params.id) {
 
         try {  // this try catch is for deleting all the post of that user which are deleting thier account
-              
+
             const user = await User.findById(req.params.id)
 
             try {         // this try catch is for delete user 
@@ -172,15 +172,15 @@ exports.deleteuser = async (req, res) => {
                 });
 
             }
-        } catch (error) { 
+        } catch (error) {
             console.log(error.message)
             res.status(404).json({
-                message:"user not found!",
+                message: "user not found!",
                 success: false,
                 message: "error in delete controller in first try catch ",
-              
+
             });
-         }
+        }
 
     }
     else {
@@ -190,16 +190,16 @@ exports.deleteuser = async (req, res) => {
 
 
 // function for get a single user 
-exports.getUser = async (req,res) => {
+exports.getUser = async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
         const { password, ...userInformation } = user._doc;  // we are fetchin all the information excluding user password
         res.status(201).json({
-            success:true,
-            message:"user found succesfully",
+            success: true,
+            message: "user found succesfully",
             userInformation
         })
-        
+
     } catch (error) {
         console.log(error.message)
         res.status(401).json({
@@ -213,51 +213,50 @@ exports.getUser = async (req,res) => {
 
 // function for creating post  // allmost same as /register  in /register i am using .save for saving data in datavase but here i will used .create for saving data inside databse
 
-exports.createPost = async (req,res) =>{
+exports.createPost = async (req, res) => {
 
     try {
-       const {username, title, description}  = req.body
-        if(!username){
+        const { username, title, description } = req.body
+        if (!username) {
             return res.status(404).send("username is required")
         }
-        if(!title){
+        if (!title) {
             return res.status(404).send("title is required to crete a post")
         }
 
-        const savePost = await  Post.create({username,title,description});
+        const savePost = await Post.create({ username, title, description });
         res.status(201).json({
             success: true,
             message: "Post is succesfuuly creted",
             savePost
         })
-        
+
     } catch (error) {
         res.status(201).json({
             success: false,
             message: "error in creat post controller"
         })
-        
+
     }
 }
 
-//
-
+//function for update the post details
 exports.updatePost = async (req, res) => {
     try {
-        const post = await Post.findById(req.params.id);  
-        const {username , title} = req.body
-        if(!username){
-            return res.status(401).send("username is required to update the title of post")
+        const post = await Post.findById(req.params.id);
+        const { username, title } = req.body
+        if (!username) {
+            return res.status(401).send("username is required to update the title")
         }
-        if(post.username == username){
+        if (post.username == username) {
             try {
-              const titleexist = await Post.findOne({title})
-              if(titleexist){
-                return res.status(401).send("Please write another title this title is already in use")
-              }
-                const updatedPost = await Post.findByIdAndUpdate(req.params.id , {
+                const titleexist = await Post.findOne({ title })
+                if (titleexist) {
+                    return res.status(401).send("Please write another title this title is already in use")
+                }
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
                     $set: req.body
-                }, {new:true} )
+                }, { new: true })
                 res.status(201).json({
                     success: true,
                     message: "post is succsesfully updated",
@@ -268,21 +267,83 @@ exports.updatePost = async (req, res) => {
                     success: false,
                     message: "error in edit post controller"
                 })
-            }    
+            }
         }
-        else{
+        else {
             res.status(401).json({
                 success: false,
-                message: "You can update only your post!" ,
-                 a:post.username,
-                 b:username
+                message: "You can update only your post!",
+                a: post.username,
+                b: username
             })
-            
+
         }
 
-        
+
     } catch (error) {
         res.status(500).send(error)
     }
- 
+
 }
+
+//function for delete the post details
+exports.deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        const { username, title } = req.body
+        if (!username) {
+            return res.status(401).send("username is required to delete the title")
+        }
+        if (post.username == username) {
+            try {
+                await post.delete();
+                res.status(201).json({
+                    success: true,
+                    message: "post is deleted updated",
+                })
+            } catch (error) {
+                console.log(error.message)
+                console.log("error in deletePost controller")
+                res.status(201).json({
+                    success: false,
+                    message: "error in deletePpost controller"
+                })
+            }
+        }
+        else {
+            res.status(401).json({
+                success: false,
+                message: "You can delete only your post!",
+                a: post.username,
+                b: username
+            })
+
+        }
+
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+}
+
+// function for getting the post  - almost same like getUser
+exports.getPost = async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+res.status(201).json({
+    success: true,
+    post,
+})
+        
+    } catch (error) {
+        console.log(error.message)
+        console.log("error in getPost controller")
+        res.status(201).json({
+            success: false,
+            message: "error in getPost controller"
+        })
+    }
+}
+
+
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {home , register , login, update , deleteuser , getUser , createPost , updatePost } = require("../controller/userController")
+const {home , register , login, update , deleteuser , getUser , createPost , updatePost ,deletePost, getPost } = require("../controller/userController")
 
 router.get("/",home)
 router.post("/register",register)
+
 router.post("/login",login)
 router.put("/:id",update)
 router.delete("/:id", deleteuser)
@@ -12,6 +13,8 @@ router.get("/:id", getUser)
 
 router.post("/post",createPost)
 router.put("/post/:id",updatePost)
+router.delete("/post/delete/:id",deletePost)
+router.get("/getpost/:id",getPost)
 
 
 module.exports = router;
