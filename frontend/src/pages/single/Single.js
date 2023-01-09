54fd54b36fea7d7bd76c539122ecfe186267a1fa
amarjeetk123 <ak123del@gmail.com>
2023-01-09 22:34:23 +0530

    removing sidebar from sinle.js

--- frontend/src/pages/single/Single.js
+++ frontend/src/pages/single/Single.js
@@ -1,12 +1,10 @@
 import  "./Single.css"
-import Sidebar from "../../components/sidebar/Sidebar"
 import SinglePost from "../../components/singlePost/SinglePost"
 
 const Single = () => {
   return (
     <div className="single" >
         <SinglePost />
-        <Sidebar />
     </div>
   )
 }
